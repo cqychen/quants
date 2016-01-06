3fce97b87133de700c7bd5911109c5e29e888f7c
cqychen <chen_yu_qin_g@163.com>
2016-01-06 20:43:08 +0800

    开始编写main脚本

--- start.py
+++ start.py
@@ -3,8 +3,30 @@ Created on 2016年1月4日
 
 @author: cqychen
 '''
+import time as dt
+import loaddata.loadcompanydata as lc
 
 if __name__ == '__main__':
+    '''
+    程序运行开始计时
+    '''
     print("======================start to go=======================")
+    print("=================task start,calculate start time==================")
+    start_time=dt.time()
+    start_date_formate=dt.strftime("%Y-%m-%d %H:%M:%S",dt.localtime())
+    print("start time is :",start_date_formate," start seconds is :",start_time)
+    print("===================================================================")
     
+    '''
+    运行代码区域，调用数据下载接口，计算接口，报表短信推送接口。
+    '''
+    lc.company_industry_data();
+    '''
+    程序运行结束计时
+    '''
+    print("===========================task finished,caculate the end time ====================")
+    end_time=dt.time()
+    end_date_formate=dt.strftime("%Y-%m-%d %H:%M:%S",dt.localtime())
+    print("start time is :",end_date_formate," start seconds is :",end_time," the program cost :",end_time-start_time,"seconds")
+    print("===================================================================================")
     
