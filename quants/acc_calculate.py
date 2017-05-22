07155a0708c9b36c02f02bdf37634c762af0a0af
chenyuqing <chenyuqing@didichuxing.com>
2017-05-22 13:59:04 +0800

    组织调整，new start。

--- quants/acc_calculate.py
+++
@@ -1,5 +0,0 @@
-'''
-Created on 2016年1月7日
-本脚本主要是聚合策略算法，比如因子分析，指标初始化等等
-@author: Administrator
-'''
--- start-cmd.bat
+++
@@ -1,2 +0,0 @@
-python E:\quant\quants\skyeye_loaddata_basic_stock_info.py
-python E:\quant\quants\skyeye_loaddata_trade_hdata_stock_info.py
