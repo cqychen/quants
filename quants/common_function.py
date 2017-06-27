5f923161bde0044169c687f2e96a87b1e96ba5cd
chenyuqing <chenyuqing@didichuxing.com>
2017-06-27 17:36:47 +0800

    修改函数

--- quants/common_function.py
+++ quants/common_function.py
@@ -25,9 +25,9 @@ def get_stock_info():
     从companyclassified中提取股票信息，这个表会每天进行更新，以获取最新的数据
     包括股票代码，上市日期，市盈率等信息
     '''
-    sql_str="SELECT *  FROM  stock_company.`company_basic_info`"
+    sql_str="SELECT *  FROM  stock_company.`ods_company_basic_info`"
     iphost, user, passwd = get_mysql_conn()
-    db='stock_company'
+    db='ods_data'
     charset='utf8'
     conn = pymysql.connect(user=user, passwd=passwd,host=iphost, db=db,charset=charset)
     rs=sql.read_sql_query(sql=sql_str, con=conn, index_col='code', coerce_float=True)
