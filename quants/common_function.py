709cd5449276bad33196c264aaf398ed7406af59
cqychen <chen_yu_qin_g@163.com>
2017-06-28 18:10:03 +0800

    添加公共方法

--- quants/common_function.py
+++ quants/common_function.py
@@ -32,13 +32,61 @@ def get_stock_info():
     conn = pymysql.connect(user=user, passwd=passwd,host=iphost, db=db,charset=charset)
     rs=sql.read_sql_query(sql=sql_str, con=conn, index_col='code', coerce_float=True)
     return rs
-
 def run_mysql_cmd(cmd,conn):
+    '''
+    :param cmd:执行的cmd命令 
+    :param conn: 连接情况
+    :return: 
+    '''
     cmd=cmd
     conn=conn
     cur=conn.cursor()
     cur.execute(cmd)
     conn.commit()
+    return cur.fetchall()
+
+
+def get_max_date_stock(stock_code):
+    '''
+    :param stock_code:输入股票代码 
+    :return: 得到股票代码的最新数据日期
+    '''
+    cmd='''
+    select max(date) 
+    from ods_data.ods_tra_day_k
+    where code='%s'
+    '''%stock_code
+
+    iphost,user,passwd=get_mysql_conn()
+    db='ods_data'
+    charset='utf8'
+    conn = pymysql.connect(user=user, passwd=passwd,host=iphost, db=db,charset=charset)
+    return run_mysql_cmd(cmd, conn)[0][0]
+
+
+def get_min_date_stock(stock_code):
+    '''
+    :param stock_code:输入股票代码 
+    :return: 得到股票代码的最新数据日期
+    '''
+    cmd = '''
+    select min(date) 
+    from ods_data.ods_tra_day_k
+    where code='%s'
+    ''' % stock_code
+
+    iphost, user, passwd = get_mysql_conn()
+    db = 'ods_data'
+    charset = 'utf8'
+    conn = pymysql.connect(user=user, passwd=passwd, host=iphost, db=db, charset=charset)
+    return run_mysql_cmd(cmd, conn)[0][0]
+
+if __name__ == '__main__':
+    #--------------------设置基本信息---------------------------------
+    print("--------------main 函数测试-----------------------------")
+    print  get_min_date_stock(stock_code='000001')
+
+
 
 
 
