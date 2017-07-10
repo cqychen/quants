55b06bfdc0c3dafb0894db1f1613bf71bf7f7413
cqychen <chen_yu_qin_g@163.com>
2017-07-10 20:04:16 +0800

    上传投资分配预案代码

--- quants/loaddata/skyeye_ods_invest_refer_achi_forcast.py
+++ quants/loaddata/skyeye_ods_invest_refer_achi_forcast.py
@@ -20,12 +20,12 @@ def create_table(table_name):
     cmd='''
     create table if not exists %s
     (
-    code             VARCHAR (10)  comment '股票代码'
+    `code`           VARCHAR (10)  comment '股票代码'
     ,`name`          VARCHAR (63)  comment '股票名称'
     ,`type`          VARCHAR (63)  comment '业绩类型'
     ,report_date     VARCHAR (63)  comment '发布日期'
     ,pre_eps         DOUBLE        comment '上年同期每股收益'
-    ,range           DOUBLE        comment '业绩变动范围'
+    ,`range`         DOUBLE        comment '业绩变动范围'
     ,`year`          VARCHAR (63)  comment '业绩年份'
     ,`quarter`       VARCHAR (63)  comment '业绩季度'
     ,PRIMARY KEY(code,report_date)
@@ -35,23 +35,27 @@ def create_table(table_name):
     print (cmd)
     run_mysql_cmd(cmd,conn)
 def get_data_date(year,quarter):
-    cmd='''delete  from %s where `year`=%s and quarter=%s '''%(table_name,year,quarter)
+    cmd='''delete  from %s where `year`='%s' and quarter='%s' '''%(table_name,year,quarter)
     print cmd
     run_mysql_cmd(cmd=cmd,conn=conn) #先删除指定年和指定季度的数据
     rs=ts.forecast_data(year=int(year),quarter=int(quarter))
     rs['year']=year
     rs['quarter']=quarter
+    pd.DataFrame.to_sql(rs, table_name, con=conn , flavor='mysql', if_exists='append',index=False)
     return rs
 def load_data():
     #下载公司基本信息，包括股票代码、pe、市盈率等数据
-    max_year=get_max_year_achi_forcast() #还要获取前一年的情况
+    max_year=int(get_max_year_achi_forcast()) #还要获取前一年的情况
     (year,quarter,mon,day,hour,min,sec,wday,yday,isdst)=get_date_struct()
     while True:
+        print(max_year,year)
         if(max_year<=year):
             print("geting year is %s"%max_year)
-            rs=get_data_date(year=max_year)
+            get_data_date(year=max_year,quarter=1)
+            get_data_date(year=max_year,quarter=2)
+            get_data_date(year=max_year,quarter=3)
+            get_data_date(year=max_year,quarter=4)
             print("\n\n")
-            pd.DataFrame.to_sql(rs, table_name, con=conn , flavor='mysql', if_exists='append',index=False)
             max_year=max_year+1
         else:
             break
