6b0746bb2284a4c3c57de6073d44232d24b39502
cqychen <chen_yu_qin_g@163.com>
2017-07-26 19:33:36 +0800

    合并股票分类信息

---
+++ quants/DW/dwd_classified_info.sql
@@ -0,0 +1,22 @@
+use ods_data;
+CREATE TABLE dwd_classified_info as
+select
+a.`code`
+,a.`name`
+,a.c_name  industry
+,b.area
+,case when d.`code` is not null then '创业板' else '非创业板' end  isgem
+,case when e.`code` is not null then '沪深300' else '非沪深300' end  ishs300
+,case when f.`code` is not null then '中小板' else '非中小板' end  issme
+,case when g.`code` is not null then 'st' else 'nost' end  isst
+,case when h.`code` is not null then 'sz50' else 'nosz50' end  issz50
+,case when j.`code` is not null then 'zz500' else 'nozz500' end  iszz500
+from ods_classified_basic a
+left JOIN ods_classified_area b on(a.`code`=b.`code`)
+LEFT JOIN ods_classified_gem d  ON(a.`code`=d.`code`)
+LEFT JOIN ods_classified_hs300 e ON(a.`code`=e.`code`)
+LEFT JOIN ods_classified_sme f  on(a.`code`=f.`code`)
+LEFT JOIN ods_classified_st g on(a.`code`=g.`code`)
+LEFT JOIN ods_classified_sz50s h ON(a.`code`=h.`code`)
+LEFT JOIN ods_classified_zz500s j ON(a.`code`=j.`code`)
+;
